423166faed94a3f4354da2c68112e7b7c6af3f7b
Foat Sharafutdinov <fsharafutdinov@cloudally.com>
2021-09-28 17:33:54 +0300

    changed to REST ideology

--- src/main/java/com/foat/secrettransfer/controller/MainController.java
+++ src/main/java/com/foat/secrettransfer/controller/MainController.java
@@ -2,24 +2,29 @@ package com.foat.secrettransfer.controller;
 
 import com.foat.secrettransfer.service.SecretService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/secret")
 public class MainController {
 	
 	@Autowired
 	private SecretService service;
 	
-	@GetMapping("/getsecret({secretId})")
+	@GetMapping("/{secretId}")
 	public String getSecret(@PathVariable("secretId") String secretId) {
 		return service.getSecret(secretId).orElse("There is no secret!");
 	}
 	
-	@PostMapping("/savesecret")
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public String saveSecret(@RequestBody String message) {
 		return service.saveSecret(message);
 	}
